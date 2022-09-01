108d77dee93a252ec293ce4672bf381f9ef442e1
Yueuwu <vladdenega1@gmail.com>
2022-09-01 13:38:49 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(".\haarcascades\haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier(".\haarcascades\haarcascade_eye.xml")
+#change to gray
+img = cv2.imread(".\somefaces\oneface1.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#detect objects
+faces = face_cascade.detectMultiScale(gray, 1.8, 5)
+#highlight objects
+for x,y,w,h in faces:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+    # looking for eyes
+    roi_color = img[y:y + h, x:x + w]
+    roi_gray = gray[y:y + h, x:x + w]
+    eyes = eye_cascade.detectMultiScale(roi_gray, 1.2, 1)
+    for ex, ey, ew, eh in eyes:
+        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+# display window with img
+cv2.imshow('Face detected', img)
+#close window after click
+cv2.waitKey(0)
+cv2.destroyAllWindows()
